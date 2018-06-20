4a867aa819e90518cbb227766b0505c342a2e060
AdamClmns <adam@adamclmns.com>
2018-06-19 21:24:57 -0500

    Initial commit

---
+++ src/main/java/com/adamclmns/pdfboxservice/APIController.java
@@ -0,0 +1,33 @@
+package com.adamclmns.pdfboxservice;
+
+import org.apache.pdfbox.io.IOUtils;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import java.util.Date;
+import java.util.UUID;
+
+@RestController
+public class APIController {
+
+    @RequestMapping(path="/version")
+    public String getVersion(){
+        Date now = new Date();
+        return "Version 1.0.0 as of "+now.toString();
+    }
+
+    @RequestMapping(path="/pdf")
+    public void getPDF(HttpServletResponse response) throws Exception{
+        PDFBuilder pdfBuilder = new PDFBuilder();
+        PDDocument pdf =  pdfBuilder.showTextWithPositioningDemo();
+        pdf.save(response.getOutputStream());
+        response.flushBuffer();
+    }
+}
---
+++ src/main/java/com/adamclmns/pdfboxservice/PdfBoxServiceApplication.java
@@ -0,0 +1,12 @@
+package com.adamclmns.pdfboxservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfBoxServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PdfBoxServiceApplication.class, args);
+    }
+}
